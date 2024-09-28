1d596d811da83e761bb23964a2ad9929d95266ce
Cha0s_MnK <kczdggyx@gmail.com>
2024-09-28 14:54:46 +0800

    modified:   Radiative Processes in Astrophysics/Planck_spec_ChuizhengKong.pdf 	modified:   Radiative Processes in Astrophysics/Planck_spec_ChuizhengKong.py 	modified:   Statistics and Numerical Methods/L02_Architecture.pdf 	renamed:    Statistics and Numerical Methods/03-InterpIntegral.pdf -> Statistics and Numerical Methods/L03_InterpIntegral.pdf 	renamed:    Statistics and Numerical Methods/04-LinearAlgebra-1.pdf -> Statistics and Numerical Methods/L04_LinearAlgebra1.pdf

--- Radiative Processes in Astrophysics/Planck_spec_ChuizhengKong.py
+++ Radiative Processes in Astrophysics/Planck_spec_ChuizhengKong.py
@@ -5,7 +5,7 @@ Usage:
     python3.9 Planck_spec_ChuizhengKong.py
 
 Version:
-    Last edited by Cha0s_MnK on 2024-09-26 (UTC+08:00).
+    Last edited by Cha0s_MnK on 2024-09-28 (UTC+08:00).
 """
 
 #########################
@@ -20,36 +20,49 @@ plt.rcParams.update({
     'xtick.direction': 'in',
     'ytick.direction': 'in'
 })
+plt.rcParams['text.usetex'] = True
+from scipy.constants import h, c, k
+k_B = k
 
+"""
 # physical constants
-h   = 6.62607015e-34 # Planck constant [J*s]
+h   = 6.62607015e-34 # Planck constant [J·s]
 c   = 2.99792458e8   # speed of light [m/s]
 k_B = 1.380649e-23   # Boltzmann constant [J/K]
-
+"""
 ###################
 # SET ARGUMENT(S) #
 ###################
 
+# figure size
+figsizeX = 9
+figsizeY = 6
+
 # X and Y limits
 lgXmin = -12
 lgXmax = 1
-Ymin = 1e-13
-Ymax = 1e39
+Ymin   = 1e-13
+Ymax   = 1e39
+
+# JWST observable range
+JWSTlambdaStart = 6.0e-7
+JWSTlambdaStop  = 2.83e-5
+JWSTposX        = 4.3e-6
 
 ######################
 # HELPER FUNCTION(S) #
 ######################
 
-def Planck_law(Lambda, T):
+def Planck_law(Lambda: np.ndarray, T: float) -> np.ndarray:
     """
-    Calculate the spectral radiance of a black body at a given wavelength and temperature.
-    
+    Calculate the specific intensity of a black body at a given wavelength and temperature.
+
     Parameter(s):
-    - Lambda (float): wavelength [m]
-    - T      (float): temperature [K]
-    
+    - Lambda   (np.ndarray): wavelength array [m]
+    - T        (float):      temperature [K]
+
     Return(s):
-    - Spectral radiance in erg/s/cm^2/cm/sr
+    - I_Lambda (np.ndarray): specific intensity [W/m^3]
     """
     return (2.0 * h * c**2) / (Lambda**5) / (np.exp((h * c) / (Lambda * k_B * T)) - 1.0)
 
@@ -59,19 +72,23 @@ def Planck_law(Lambda, T):
 
 def main():
     # temperature and wavelength ranges
-    Ts = [3 * 10**i for i in range(9)] # temperature range: 3e0, 3e1, 3e2, ..., 3e8 K
-    Lambdas_m = np.logspace(lgXmin, lgXmax, num=1000) # wavelength range: 1e-12, 1e-11, 1e-10, ..., 1e0 m
+    Ts      = 3 * np.logspace(0, 8, num=9, base=10) # temperature range: 3.0e0, 3.0e1, ..., 3.0e8 K
+    Lambdas = np.logspace(lgXmin, lgXmax, num=1000) # wavelength range: 1.0e-12, 1.0e-11, ..., 1.0e0 m
 
-    # plot
-    plt.figure(figsize=(10, 7))
-    colours = plt.cm.viridis(np.linspace(0, 1, len(Ts)))
+    # plot Planck spectrum for various temperatures
+    plt.figure(figsize=(figsizeX, figsizeY))
+    colours = plt.cm.tab10(np.linspace(0, 1, len(Ts)))
     for T, colour in zip(Ts, colours):
-        plt.plot(Lambdas_nm, Planck_law(Lambdas_m, T), label=f"{T} K", color=colour)
+        plt.plot(Lambdas, Planck_law(Lambdas, T), label=f"{T:.1e} K", color=colour)
+
+    # add JWST observable wavelength (6.0e-7 - 2.83e-5 m)
+    plt.axvspan(JWSTlambdaStart, JWSTlambdaStop, color='gray', alpha=0.3, label='JWST observable range')
+    plt.text(JWSTposX, plt.ylim()[1], f'({JWSTlambdaStart}, {JWSTlambdaStop})', color='black', fontsize=10, ha='center', va='bottom')
 
     # plot settings
-    plt.xlabel("Wavelength / nm")
-    plt.ylabel("Spectral Radiance (erg/s/cm²/cm/sr)")
-    plt.title("Planck Spectrum for Various Temperatures")
+    plt.xlabel(r'Wavelength $\lambda$ (m)')
+    plt.ylabel(r'Specific Intensity $I_\lambda$ (W·m$^{-3}$)')
+    plt.title('Planck Spectrum for Various Temperatures')
     plt.xscale('log')
     plt.yscale('log')
     plt.xlim(10 ** lgXmin, 10 ** lgXmax)
